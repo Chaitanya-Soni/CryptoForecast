1c08aa84833b0be1cd32dd07173ca7f501dd9be7
Chaitanya-Soni <201951046@iiitvadodara.ac.in>
2021-11-12 23:48:18 +0530

    added sentiment analysis update

--- CryptoforeCast/settings.py
+++ CryptoforeCast/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'CryptoforeCast.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "cryptoforecastdb",
+        'USER':'cryptoforecastdbadmin',
+        'PASSWORD':'CRYPTO@#forecast123',
+        'HOST':'cryptoforecastdb.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
